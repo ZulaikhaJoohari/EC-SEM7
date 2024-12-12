731e4e63302df3f715acddc463c1448f80aba12f
Siti Zulaikha <94262099+ZulaikhaJoohari@users.noreply.github.com>
2024-12-12 23:25:11 +0800

    Update assignment1.py

--- pages/assignment1.py
+++ pages/assignment1.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("Scheduling using Genetic Algorithm")
+st.dataframe
 
 # Function to read the CSV file and convert it to the desired format
 def read_csv_to_dict(file_path):
